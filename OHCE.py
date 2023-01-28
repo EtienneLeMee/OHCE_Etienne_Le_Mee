b0c640f1c5e193c3159145d1a16d79d3805487d3
Etienne Le Mée <et.lemee@gmail.com>
2023-01-28 23:10:41 +0100

    Partie 1.3 - commit 2
    function bonjour

--- OHCE.py
+++ OHCE.py
@@ -1,7 +1,14 @@
 class OHCE:
+
     def miroir(string):
         return string[::-1]
 
+    def bien_dit(self):
+        return "Bien dit !"
+    def bonjour(self):
+        return "Bonjour"
     def palindrome(string):
-        return OHCE.miroir(string) \
-            + "Bien dit !"
+        ohce = OHCE()
+        return OHCE.bonjour(ohce) \
+            + OHCE.miroir(string) \
+            + OHCE.bien_dit(ohce)
