37c4bd869838798b33e297ace9dd13ea9e83ad3d
Etienne Le Mée <et.lemee@gmail.com>
2023-01-28 23:17:39 +0100

    Partie 1.4 - commit 2
    function au_revoir

--- OHCE.py
+++ OHCE.py
@@ -8,8 +8,11 @@ class OHCE:
     def bonjour(self):
         return "Bonjour"
 
+    def au_revoir(self):
+        return "Au revoir !"
     def palindrome(string):
         ohce = OHCE()
         return OHCE.bonjour(ohce) \
             + OHCE.miroir(string) \
-            + OHCE.bien_dit(ohce)
+            + OHCE.bien_dit(ohce) \
+            + OHCE.au_revoir(ohce)
