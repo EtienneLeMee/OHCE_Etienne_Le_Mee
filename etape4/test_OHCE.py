2a2c941d2f623d69a9c40365d3fae8403b681cc0
Etienne Le Mée <et.lemee@gmail.com>
2023-01-31 18:06:04 +0100

    Etape 4 - commit 3
    Test heure de la journée

--- etape4/test_OHCE.py
+++ etape4/test_OHCE.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime
 from unittest.mock import patch
-
+from datetime import datetime
 import locale
 
 import OHCE
@@ -17,6 +17,18 @@ class OHCETest(unittest.TestCase):
         langue = locale.getdefaultlocale()[0][:2]
         #Et la période de la journée
         heure = "08:00:00"
+        #Quand on saisit une chaîne
+        palindrome = "kayak"
+        # Alors bonjour dans cette langue et à cette periode est renvoyé avant toute réponse
+        self.assertIn(bonjour, ohce.palindrome_langue_periode(palindrome,langue,heure)[:len(bonjour)])
+
+    def test_horloge(self):
+        bonjour = "Bonjour"
+        #Etant donné un utilisateur parlant une langue
+        langue = locale.getdefaultlocale()[0][:2]
+        #Et la période de la journée
+        currentDateAndTime = datetime.now()
+        heure = currentDateAndTime.strftime("%H:%M:%S")
         #Quand on saisit une chaîne
         palindrome = "kayak"
         # Alors bonjour dans cette langue et à cette periode est renvoyé avant toute réponse
