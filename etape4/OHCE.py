2af273e3e6c75261117d419b54a9084ef3e842ef
Etienne Le Mée <et.lemee@gmail.com>
2023-01-31 11:03:04 +0100

    Partie 4 - commit 1
    test langue

---
+++ etape4/OHCE.py
@@ -0,0 +1,60 @@
+class OHCE:
+
+    def miroir(string):
+        return string[::-1]
+
+    def bien_dit(self):
+        return "Bien dit !"
+
+    def bien_dit_langue(self,langue):
+        if langue=="fr":
+            return "Bien dit !"
+        elif langue=="en":
+            return "Well done !"
+
+    def bonjour_langue_heure(self,langue,heure):
+        if langue=="fr":
+            if heure <= "12:00:00":
+                return "Bonjour"
+            elif heure <= "18:00:00":
+                return "Bon apres-midi"
+            elif heure <= "21:00:00":
+                return "Bonsoir"
+            else:
+                return "Bonne soirée"
+        elif langue=="en":
+            if heure <= "12:00:00":
+                return "Hello"
+            elif heure <= "18:00:00":
+                return "Good afternoon"
+            elif heure <= "21:00:00":
+                return "Good evening"
+            else:
+                return "Good night"
+
+    def aurevoir_langue_heure(self, langue, heure):
+        if langue == "fr":
+            if heure <= "12:00:00":
+                return "Au revoir !"
+            elif heure <= "18:00:00":
+                return "Bon fin d'apres-midi !"
+            elif heure <= "21:00:00":
+                return "Bonne fin de soirée !"
+            else:
+                return "Bonne nuit !"
+        elif langue == "en":
+            if heure <= "12:00:00":
+                return "Bye !"
+            elif heure <= "18:00:00":
+                return "Good afternoon, see you soon !"
+            elif heure <= "21:00:00":
+                return "Good evening, see you soon !"
+            else:
+                return "Good night, see you soon !"
+
+    def palindrome_langue_periode(string,langue,heure):
+        ohce = OHCE()
+        return OHCE.bonjour_langue_heure(ohce,langue,heure) \
+            + OHCE.miroir(string) \
+            + OHCE.bien_dit_langue(ohce,langue) \
+            + OHCE.aurevoir_langue_heure(ohce,langue,heure)
---
+++ etape4/test_OHCE.py
@@ -0,0 +1,27 @@
+import unittest
+from datetime import datetime
+from unittest.mock import patch
+
+import OHCE
+from OHCE import OHCE as ohce
+import os
+
+class OHCETest(unittest.TestCase):
+    print("DEBUT TEST")
+
+    def test_langue_systeme(self):
+        bonjour = "Bonjour"
+        #Etant donné un utilisateur parlant une langue
+        langue = os.getenv('LANG')
+        #Et la période de la journée
+        heure = "08:00:00"
+        #Quand on saisit une chaîne
+        palindrome = "kayak"
+        # Alors bonjour dans cette langue et à cette periode est renvoyé avant toute réponse
+        self.assertIn(bonjour, ohce.palindrome_langue_periode(palindrome,langue,heure)[:len(bonjour)])
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
